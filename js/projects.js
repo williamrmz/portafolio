20a27be41adc50e8bb8559b8ec65b71a047bda9d
William <williamxtez14@gmail.com>
2020-10-15 17:06:46 -0500

    sin arreglar imgs app

--- js/projects.js
+++ js/projects.js
@@ -16,9 +16,9 @@ function navegador() {
 let data = [
     {
         titulo: "Ganimed Web",
-        tipo: 1,
+        tipo: "web",
         imgportada: "img/gan-web/Listado.png",
-        description: `Una SPA (Single Page Application) desarrollada para consultorio pediatra de la clínica Ganimed para la detección de anemia en pacientes de 0 a 12 años
+        description: `Una SPA (Single Page Application) desarrollada para consultorio pediatra de la clínica Ganimed para la detección de anemia en pacientes de 0 a "web"2 años
 			a partir de los datos obtenidos en su examinación. Para los niños detectados con anemia se le considera un segundo exámen de diagnóstico luego de dos meses 
 			para evaluar mejorías.
 			Las tecnologías usadas son Angular para el frontend, NodeJS con Express para el backend con API Rest, PostgreSQL para
@@ -28,7 +28,7 @@ let data = [
     },
     {
         titulo: "Ganimed App",
-        tipo: 2,
+        tipo: "app",
         imgportada: "img/gan-app/Listado.jpg",
         description:
             "Una app móvil desarrollada para consultorio pediatra de la clínica Ganimed para la detección de anemia en pacientes de 0 a 12 años",
@@ -37,12 +37,12 @@ let data = [
     },
     {
         titulo: "Samuel Johnson",
-        tipo: 1,
+        tipo: "web",
         imgportada: "img/sj/Inicio.png",
         description: "Página web desarrollada para la academia de inglés Samuel Johnson",
         imggallery: ["img/sj/Inicio.png", "img/sj/Nosotros.png", "img/sj/Comunidad.png"],
         tags: ["JavaScript", "CSS", "Bootstrap", "jQuery"],
-    },
+    }
 ];
 
 /* CARDS */
@@ -63,7 +63,7 @@ function cards() {
                         </div>					
                                 <figure class="figure">
                                     <img ${
-                                        project.tipo == 1
+                                        project.tipo == "web"
                                             ? ` class="cargando imgweb"`
                                             : `class="cargando imgapp"`
                                     }
@@ -81,18 +81,22 @@ function cards() {
 
 // Loader en imagenes
 function loader() {
+    //ocultar figures
     figuras = document.getElementsByClassName("figure");
     Array.from(figuras).forEach((figure) => {
         figure.style.display = "none";
     });
 
+    //extraer todos los img para poder asignar el evento load
     imagenes = document.getElementsByClassName("cargando");
     Array.from(imagenes).forEach((imagen) => {
         imagen.addEventListener("load", () => {
+            //ocultar el loader una vez carguen
             const loader = document.getElementsByClassName("lds-roller");
             Array.from(loader).forEach((load) => {
                 load.style.display = "none";
             });
+            //mostrar los figure una vez carguen
             Array.from(figuras).forEach((figura) => {
                 figura.style.display = "revert";
             });
@@ -106,24 +110,35 @@ function modal() {
     const cerrar = document.getElementsByClassName("close");
     const modal = document.getElementsByClassName("modal");
     const modalC = document.getElementsByClassName("modal-container");
+    const imgs = document.getElementsByClassName("img-galeria")
 
+    //evento click para cada card y cargar datos con su index en json
     Array.from(abrir).forEach((element, index) => {
         element.addEventListener("click", function (e) {
             e.preventDefault();
-
             modalC[0].style.opacity = "1";
             modalC[0].style.visibility = "visible";
             modal[0].classList.toggle("modal-close");
             document.querySelector(".modal-titulo").textContent = `${data[index].titulo}`;
             document.querySelector(".modal-descripcion").textContent = `${data[index].description}`;
+            
+            //se selecciona el index específico del objeto dentro del json
             data[index].imggallery.map(
-                (imagen, idx) => (document.getElementById(`img-${idx}`).src = `${imagen}`)
+                (ruta, idx) => (document.getElementById(`img-${idx}`).src = `${ruta}`)
             );
+
+            //recorrido a cantidad de imagenes para agregar si es imagen app o web
+            Array.from(imgs).forEach((element, idx) => {
+                document.getElementById(`img-${idx}`).classList.remove("web", "app")
+                document.getElementById(`img-${idx}`).classList.add(`${data[index].tipo}`)
+            });
+            
             glide.update();
             glide.go("=0");
         });
     });
 
+    //evento al botón close
     cerrar[0].addEventListener("click", function (e) {
         modal[0].classList.toggle("modal-close");
         setTimeout(() => {
@@ -132,6 +147,7 @@ function modal() {
         }, 500);
     });
 
+    //evento afuera del modal
     window.addEventListener("click", function (e) {
         if (e.target == modalC[0]) {
             modal[0].classList.toggle("modal-close");
@@ -143,25 +159,25 @@ function modal() {
     });
 }
 
-function slider() {
-    var glide = new Glide(".glide", {
-        startAt: "0",
-        focusAt: "0",
-        autoplay: 3500,
-        hoverpause: true,
-        keyboard: true,
-        animationDuration: 1500,
-    });
-
-    glide.mount();
-}
-
 function funciones() {
     navegador();
     cards();
     loader();
     modal();
-    slider();
 }
 
 funciones();
+
+//librería glide
+var glide = new Glide(".glide", {
+    startAt: "0",
+    focusAt: "0",
+    autoplay: 3500,
+    hoverpause: true,
+    keyboard: true,
+    animationDuration: 1500,
+});
+
+glide.mount();
+
+
